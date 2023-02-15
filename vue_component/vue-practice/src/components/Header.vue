897b49135f628a186cb79595f3c68903d7f36de4
Shao Fu Chen <abcdmarcus@gmail.com>
2023-02-15 17:40:19 +0800

    加入vue component的練習

---
+++ vue_component/vue-practice/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @click="onClick()" text="Add Text" color="green"/>
+
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue"
+    export default {
+        name:'Header',
+        props:['title'],
+        components:{
+            Button
+        },
+        methods:{
+            onClick:function(){
+                console.log("Click!")
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display:flex;
+        justify-content: space-between;
+        align-content: center;
+        margin-bottom: 20px;
+    }
+</style>
